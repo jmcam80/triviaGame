a8e78ee75b63af230fc1c77b7b7cabedeaf54512
jmcam80 <jmcam80@gmail.com>
2018-02-14 23:27:03 -0700

    finished all the forms for the quiz for each section

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    $("#trinityForm").css("display", "none");
+    $("#morpheusForm").css("display", "none");
+    $("#neoForm").css("display", "none");
+  };
+  var trin = false;
+  var morph = false;
+  var neO = false;
+// set the function on click for trinity based questions
+$("#trinity").click( function () {
+    $("#trinityForm").css("display", "block");
+    });
+
+// set the function on click for morpheus based questions
+$("#morpheus").click( function () {
+
+    $("#morpheusForm").css("display", "block");
+    
+});
+
+$("#neo").click( function () {
+
+  $("#neoForm").css("display", "block");
+    
+
+});
+
+function handleClick()
+  {         
+var amountCorrect = 0;          
+for(var i = 1; i <= 45; i++) {
+  var radios = document.getElementsByName('group'+i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if(radio.value == "correct" && radio.checked) {
+      amountCorrect++;
+    };
+  };
+ };                 
+    alert("Correct Responses: " + amountCorrect);
+}
